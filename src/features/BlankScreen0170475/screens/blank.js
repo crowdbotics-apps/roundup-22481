578cecc6753b7e224fae633c5dffb9391b4951e8
Crowdbotics <team@crowdbotics.com>
2020-11-10 17:12:33 +0000

    Update components

--- src/features/BlankScreen0170475/screens/blank.js
+++ src/features/BlankScreen0170475/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -8,33 +8,87 @@ import {
   Button,
   Switch,
   TextInput,
-  StyleSheet,
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {
+    TextInput_3: "",
+    TextInput_5: "",
+    TextInput_7: "",
+    TextInput_9: "",
+    TextInput_11: "",
+    TextInput_18: "",
+    TextInput_21: ""
+  }
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <TextInput
+        placeholder="Target.                   22.18"
+        value={this.state.TextInput_3}
+        onChangeText={nextValue => this.setState({ TextInput_3: nextValue })}
+      />
+      <TextInput
+        placeholder="       RoundUp.               + 0.82"
+        style={styles.TextInput_5}
+        value={this.state.TextInput_5}
+        onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
+      />
+      <TextInput
+        placeholder="Starbucks.             6.25"
+        value={this.state.TextInput_7}
+        onChangeText={nextValue => this.setState({ TextInput_7: nextValue })}
+      />
+      <TextInput
+        placeholder="       RoundUp.               + 0.75"
+        style={styles.TextInput_9}
+        value={this.state.TextInput_9}
+        onChangeText={nextValue => this.setState({ TextInput_9: nextValue })}
+      />
+      <TextInput
+        placeholder="Walmart.               15.62"
+        value={this.state.TextInput_11}
+        onChangeText={nextValue => this.setState({ TextInput_11: nextValue })}
+      />
+      <TextInput
+        placeholder="       RoundUp.               + 0.38"
+        style={styles.TextInput_18}
+        value={this.state.TextInput_18}
+        onChangeText={nextValue => this.setState({ TextInput_18: nextValue })}
+      />
+      <TextInput
+        placeholder="Total added towards loans with RoundUp: $1.95"
+        style={styles.TextInput_21}
+        value={this.state.TextInput_21}
+        onChangeText={nextValue => this.setState({ TextInput_21: nextValue })}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 16,
+    marginHorizontal: 16
   },
-});
+  View_1: {},
+  TextInput_3: {},
+  TextInput_5: { color: "#dc1818", fontFamily: "Merriweather-Italic" },
+  TextInput_7: {},
+  TextInput_9: { fontFamily: "Merriweather-Italic" },
+  TextInput_11: {},
+  TextInput_18: { fontFamily: "Merriweather-Italic" },
+  TextInput_21: { fontSize: 8, fontFamily: "RobotoMono-Regular" }
+})
